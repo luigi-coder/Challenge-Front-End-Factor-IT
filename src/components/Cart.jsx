6fb8bd68ef1b6428a561840272edb9af0857df4b
luigi-coder <luigicoder28@gmail.com>
2023-10-27 04:20:43 -0300

    Challenge Front End

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
 export const Cart = ({ carrito, eliminarProducto, cliente, vaciarCarrito }) => {
 
-    console.log(cliente);
+    const [timer, settimer] = useState(null);
 
-    //const [cliente, setCliente] = useState('comun');
-    // Esto tiene que venir de la base de datos de una tabla con el historial de compras del cliente
-    const gastoMesAnterior = 12000;
+    // Función para iniciar el temporizador
+    const startTimer = () => {
+
+        const timerId = setTimeout(() => {
+            vaciarCarrito();
+        }, 2 * 60 * 60 * 1000);
+
+        settimer(timerId);
+    }
+
+    // Defino un estado timer que inicie un temporizador cuando se monte el componente
+    useEffect(() => {
+        startTimer();
+    }, [])
+
+
+    // Efecto que reinicia el temporizador cada vez que el cliente realiza una accion en el carrito
+    useEffect(() => {
+        startTimer();
+    }, [carrito, vaciarCarrito]);
+
+    // Efecto que limpia el temporizador cuando el componente se desmonta para evitar memory leaks o fugas de memoria
+    useEffect(() => {
+        return () => {
+            if (timer) {
+                clearTimeout(timer); // Limpiar el temporizador cuando el componente se desmonta
+            }
+        };
+    }, [timer]);
+
 
     // Elimina los productos duplicados del carrito
     const carritoUnico = [...new Set(carrito)];
@@ -23,13 +50,13 @@ export const Cart = ({ carrito, eliminarProducto, cliente, vaciarCarrito }) => {
 
         if (cantidadProductos === 4) {
             return totalAPagar * 0.75; // Aplica el descuento del 25%
-        }else if(cantidadProductos > 10){
+        } else if (cantidadProductos > 10) {
 
             let descuento = 0;
             // Si el gastó el mes anterior fue mayor a 10000, aplica el descuento de $500
-            if(cliente === 'VIP'){
+            if (cliente === 'VIP') {
                 descuento = 500;
-            }else if(cliente === 'Promocional'){
+            } else if (cliente === 'Promocional') {
                 descuento = 300;
             }
             return totalAPagar - descuento;
@@ -38,6 +65,56 @@ export const Cart = ({ carrito, eliminarProducto, cliente, vaciarCarrito }) => {
         return totalAPagar; // Si no se cumple la condición, devuelve el total sin descuento
     }
 
+    const handlePagarClick = () => {
+        // Obtener el carrito único con productos únicos
+        const carritoUnico = [...new Set(carrito)];
+
+        // Crear un objeto para rastrear la cantidad de cada producto en la compra
+        const cantidadPorProducto = {};
+
+        // Recorrer el carrito único y contar la cantidad de cada producto
+        carritoUnico.forEach((productoUnico) => {
+            const cantidad = carrito.filter((p) => p.id === productoUnico.id).length;
+            cantidadPorProducto[productoUnico.id] = cantidad;
+        });
+
+        // Crear una lista de productos con sus cantidades
+        const compra = carritoUnico.map((productoUnico) => ({
+            producto: productoUnico,
+            cantidad: cantidadPorProducto[productoUnico.id],
+        }));
+
+        // La variable 'compra' ahora contiene la lista de productos comprados con sus cantidades
+        console.log('Compra:', compra);
+
+        // Llama a la función para registrar la compra y pasa 'compra' como argumento
+        registrarCompra(compra);
+
+        // Aquí puedes realizar otras acciones, como mostrar un mensaje de confirmación
+    };
+
+    const registrarCompra = async (compra) => {
+
+        try {
+            const response = await fetch('http://localhost:3000/compras', {
+
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(compra),
+            });
+
+            if (response.ok)
+                alert('La compra se registró correctamente.');
+
+            if (response.status === 400)
+                alert('No se pudo registrar la compra.');
+
+        } catch (error) {
+            alert('No se pudo registrar la compra.');
+        }
+    }
 
     return (
         <div>
@@ -72,7 +149,9 @@ export const Cart = ({ carrito, eliminarProducto, cliente, vaciarCarrito }) => {
             <p>Total a pagar con descuento: ${calcularTotal()}</p>
             <p>Total a pagar sin descuento ${totalAPagar}</p>
             <br />
-            <button>PAGAR</button>
+            <button
+                onClick={() => handlePagarClick()}
+            >PAGAR</button>
         </div>
     );
 };
